daaf0b6a0361ba8a8cb043c99a47c087876c7034
Glitch (my-bot-corno) <none>
2020-06-23 18:10:15 +0000

    🐴🎙 Updated with Glitch

--- events/client/ready.js
+++
@@ -1,9 +0,0 @@
-const config = require("../../config.json")
-
-module.exports = async bot => {
-    console.log(`${bot.user.username} is online`);
-
-    const api = require("../../easycode")
-    new api.oauth2(config.token,config.api)
-   await api.connect(bot)
-};
